f36daac47d47ca13ee3ff09b7581e65b948b4d14
André Camatta <andrecamatta@gmail.com>
2019-08-20 07:40:00 -0300

    Passos iniciais

---
+++ estimativas_frequencia_e_severidade.R
@@ -0,0 +1,13 @@
+##### Parte 1: Estudo da freqüência
+
+### Lendo os dados de frequência
+frequencias.entrada <- read.table("C:\\Users\\andre\\OneDrive\\Documentos\\Atuaria\\TeoriaDoRisco\\perdascarteiras\\1frequencias.txt",header=T)
+frequencias <- frequencias.entrada[,1]
+
+## Função de verossimilhança da Poisson
+vero.pois <- function(dados,param){
+  -sum(log(dpois(dados,param)))
+}
+estima.num <- nlminb(100,vero.pois,dados=frequencias,lower=0)
+estima.num$par
+
