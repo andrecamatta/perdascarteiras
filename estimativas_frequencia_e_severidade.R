6df638a72c463fd0fe98e0c9325682200d4f20a6
André Camatta <andrecamatta@gmail.com>
2019-08-20 21:38:43 -0300

    Verificando o uso de um boxplot

--- estimativas_frequencia_e_severidade.R
+++ estimativas_frequencia_e_severidade.R
@@ -9,7 +9,7 @@ vero.pois <- function(dados,param){
   -sum(log(dpois(dados,param)))
 }
 estima.num <- nlminb(100,vero.pois,dados=frequencias,lower=0)
-estima.num$par
+lambda<-estima.num$par
 
 library(fitdistrplus)
 
@@ -20,4 +20,17 @@ fpoisMLE <- fitdist(frequencias, "pois", method="mle")
 fpoisMLE
 
 summary(fpoisMLE)
+
+par(mfrow=c(1,1))
+seq=min(frequencias):max(frequencias)
+points(dpois(seq,lambda),col="red",lwd=2)
+
+frequencias.df<-data.frame(frequencias)
+frequencias.df["ocorrencias"]<-rep("Ocorrencias",length(frequencias))
+library(ggplot2)
+ggplot(data = frequencias.df) +
+  geom_boxplot(aes(x = ocorrencias, y = frequencias)) +
+  coord_flip() +
+  theme_bw()
+
 
